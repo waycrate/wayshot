da191c18a3195c19070e746d4d7f18a1c55b7f44
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-07-07 17:03:36 +0800

    chore: update clap to 4.0
    * Atty is no longer maintained.
    * Update to clap `4.0`.
    
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

--- wayshot/src/clap.rs
+++ wayshot/src/clap.rs
@@ -1,7 +1,7 @@
-use clap::{arg, Command};
+use clap::{arg, ArgAction, Command};
 
-pub fn set_flags() -> Command<'static> {
-    let app = Command::new("wayshot")
+pub fn set_flags() -> Command {
+    Command::new("wayshot")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("Screenshot tool for compositors implementing zwlr_screencopy_v1.")
@@ -9,53 +9,52 @@ pub fn set_flags() -> Command<'static> {
             arg!(-d - -debug)
                 .required(false)
                 .conflicts_with("stdout")
-                .takes_value(false)
+                .action(ArgAction::SetFalse)
                 .help("Enable debug mode"),
         )
         .arg(
             arg!(-s --slurp <GEOMETRY>)
                 .required(false)
-                .takes_value(true)
+                .action(ArgAction::SetTrue)
                 .help("Choose a portion of your display to screenshot using slurp"),
         )
         .arg(
             arg!(-f - -file <FILE_PATH>)
                 .required(false)
                 .conflicts_with("stdout")
-                .takes_value(true)
+                .action(ArgAction::SetTrue)
                 .help("Mention a custom file path"),
         )
         .arg(
             arg!(-c - -cursor)
                 .required(false)
-                .takes_value(false)
+                .action(ArgAction::SetFalse)
                 .help("Enable cursor in screenshots"),
         )
         .arg(
             arg!(--stdout)
                 .required(false)
                 .conflicts_with("file")
-                .takes_value(false)
+                .action(ArgAction::SetFalse)
                 .help("Output the image data to standard out"),
         )
         .arg(
             arg!(-e --extension <FILE_EXTENSION>)
                 .required(false)
-                .takes_value(true)
+                .action(ArgAction::SetTrue)
                 .help("Set image encoder (Png is default)"),
         )
         .arg(
             arg!(-l - -listoutputs)
                 .required(false)
-                .takes_value(false)
+                .action(ArgAction::SetFalse)
                 .help("List all valid outputs"),
         )
         .arg(
             arg!(-o --output <OUTPUT>)
                 .required(false)
-                .takes_value(true)
+                .action(ArgAction::SetTrue)
                 .conflicts_with("slurp")
                 .help("Choose a particular display to screenshot"),
-        );
-    app
+        )
 }
