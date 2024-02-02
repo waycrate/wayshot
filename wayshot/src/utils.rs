736b6515c86f5443e09aa888629c439cff1d5618
Andreas Backx <andreas@backx.org>
2024-02-02 19:01:37 +0000

    Improve CLI design
    This "improves" (and that is subjective) the design of the CLI. I am aiming to get some feedback on what people think of the new design:
    
    ```
    Screenshot tool for wlroots based compositors implementing the zwlr_screencopy_v1 protocol.
    
    Usage: wayshot [OPTIONS] [OUTPUT]
    
    Arguments:
      [OUTPUT]
              Where to save the screenshot, "-" for stdout. Defaults to "$UNIX_TIMESTAMP-wayshot.$EXTENSION"
    
    Options:
          --log-level <LOG_LEVEL>
              Log level to be used for printing to stderr
    
              [default: info]
              [possible values: trace, debug, info, warn, error]
    
      -s, --slurp <SLURP_ARGS>
              Arguments to call slurp with for selecting a region
    
      -c, --cursor
              Enable cursor in screenshots
    
          --encoding <FILE_EXTENSION>
              Set image encoder, if output file contains an extension, that will be used instead
    
              [default: png]
              [aliases: extension, format, output-format]
    
              Possible values:
              - jpg: JPG/JPEG encoder
              - png: PNG encoder
              - ppm: PPM encoder
              - qoi: Qut encoder
    
      -l, --list-outputs
              List all valid outputs
    
      -o, --output <OUTPUT>
              Choose a particular output/display to screenshot
    
          --choose-output
              Present a fuzzy selector for output/display selection
    
      -h, --help
              Print help (see a summary with '-h')
    
      -V, --version
              Print version
    ```
    
    The main changes are:
    1. `--debug` is now `--log-level` because this makes it easy to select more specifically what log level to use. I considered using `-v`, `-vv`... to increase verbosity but the `clap-verbosity-crate` uses `log` and not `tracing`. We could use it somewhat, but we'd pull in `log` (which seems fine) and we'd need to map from `log`'s Level to `tracing`'s Level enums (they use inverse ordering).
    2.  `--stdout` and `--file` has been made an optional positional argument. This because it's what other CLIs often do and I wasn't sure what to call the option otherwise because `--output` and `-O`/`-o` is often what others use but we use it here to refer to displays/monitors/Wayland outputs. This avoids that confusion hopefully and I've also clarified this in the documentation.
        * Additionally if a path is given, its extension will always be used. So you cannot save `jpg` to `foo.png`. Perhaps this behaviour can be changed, though I don't see a reason to support this weird edge case? When is someone saving `png` to `jpg`?
    3. `--extension` is `--encoding` with aliases like `extension`.
    
    Again, let me know what you think.

--- wayshot/src/utils.rs
+++ wayshot/src/utils.rs
@@ -1,6 +1,11 @@
-use eyre::{ContextCompat, Result};
+use clap::ValueEnum;
+use eyre::{bail, ContextCompat, Error, Result};
+
 use std::{
+    fmt::Display,
+    path::PathBuf,
     process::exit,
+    str::FromStr,
     time::{SystemTime, UNIX_EPOCH},
 };
 
@@ -47,16 +52,22 @@ pub fn parse_geometry(g: &str) -> Result<LogicalRegion> {
 }
 
 /// Supported image encoding formats.
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, ValueEnum)]
 pub enum EncodingFormat {
-    /// Jpeg / jpg encoder.
+    /// JPG/JPEG encoder.
     Jpg,
-    /// Png encoder.
+    /// PNG encoder.
     Png,
-    /// Ppm encoder.
+    /// PPM encoder.
     Ppm,
-    /// Qoi encoder.
+    /// Qut encoder.
     Qoi,
+}
+
+impl Default for EncodingFormat {
+    fn default() -> Self {
+        Self::Png
+    }
 }
 
 impl From<EncodingFormat> for image::ImageOutputFormat {
@@ -67,6 +78,33 @@ impl From<EncodingFormat> for image::ImageOutputFormat {
             EncodingFormat::Ppm => image::ImageFormat::Pnm.into(),
             EncodingFormat::Qoi => image::ImageFormat::Qoi.into(),
         }
+    }
+}
+
+impl TryFrom<&PathBuf> for EncodingFormat {
+    type Error = Error;
+
+    fn try_from(value: &PathBuf) -> std::result::Result<Self, Self::Error> {
+        value
+            .extension()
+            .wrap_err_with(|| {
+                format!(
+                    "no extension in {} to deduce encoding format",
+                    value.display()
+                )
+            })
+            .and_then(|ext| {
+                ext.to_str().wrap_err_with(|| {
+                    format!("extension in {} is not valid unicode", value.display())
+                })
+            })
+            .and_then(|ext| ext.parse())
+    }
+}
+
+impl Display for EncodingFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
 
@@ -81,7 +119,21 @@ impl From<EncodingFormat> for &str {
     }
 }
 
-pub fn get_default_file_name(extension: EncodingFormat) -> String {
+impl FromStr for EncodingFormat {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(match s {
+            "jpg" | "jpeg" => Self::Jpg,
+            "png" => Self::Png,
+            "ppm" => Self::Ppm,
+            "qoi" => Self::Qoi,
+            _ => bail!("unsupported extension '{s}'"),
+        })
+    }
+}
+
+pub fn get_default_file_name(extension: EncodingFormat) -> PathBuf {
     let time = match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => n.as_secs().to_string(),
         Err(_) => {
@@ -90,5 +142,5 @@ pub fn get_default_file_name(extension: EncodingFormat) -> String {
         }
     };
 
-    time + "-wayshot." + extension.into()
+    (time + "-wayshot." + extension.into()).into()
 }
