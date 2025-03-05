250a975087f87defb321b01ea990b51f77684586
Rishik-Y <73787402+Rishik-Y@users.noreply.github.com>
2025-03-05 19:34:38 +0530

    Removed all unnecessary dependency!

--- shell.nix
+++ shell.nix
@@ -4,42 +4,23 @@ with (import <nixpkgs> {});
 
 let
     libPath = with pkgs; lib.makeLibraryPath [
-    #libGL.dev
     libgbm
-    #mesa.dev
-    #egl-wayland.dev
-    wayland 
-      # load external libraries that you need in your rust project here
+    wayland
+    # load external libraries that you need in your rust project here
     ];
   moz_overlay = import /home/Kihsir/Git_Clone/nixpkgs-mozilla/rust-overlay.nix;
   rust_src_overlay = import /home/Kihsir/Git_Clone/nixpkgs-mozilla/rust-src-overlay.nix;
   # Import nixpkgs with both overlays included
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay rust_src_overlay ]; };
 
-in 
+in
   mkShell {
     name = "moz_overlay_shell";
-    buildInputs = [ 
-      stdenv.cc
+    buildInputs = [
       libGL.dev
       pkg-config
-      wlr-protocols
-      #libegl1-mesa-dev
-      libGLU.dev
-      mesa.dev
       libgbm
-      #zstd
-      #libxkbcommon
-      #vulkan-loader
-      #xorg.libX11
-      #xorg.libXcursor
-      #xorg.libXi
-      #xorg.libXrandr
-      #alsa-lib.dev
       wayland.dev
-      #egl-wayland.dev
-      udev.dev
-      # to use the latest nightly:
       nixpkgs.latest.rustChannels.nightly.rust
     ];
   LD_LIBRARY_PATH = libPath ;
