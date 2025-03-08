05de0800eaf682b23af770f85db95d150ec74f16
Rishik-Y <73787402+Rishik-Y@users.noreply.github.com>
2025-03-08 22:11:35 +0530

    Removed unnecessary comments

--- flake.nix
+++ flake.nix
@@ -11,11 +11,10 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # Define the library path
         libPath = with pkgs; lib.makeLibraryPath [
           libgbm
           wayland
-          # Add other libraries your project depends on here
+          # Add Future libraries, this project depends on here
         ];
       in
       {
@@ -31,7 +30,6 @@
             wayland.dev
           ];
 
-          # Set up environment variables for Rust
           LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
           LD_LIBRARY_PATH = libPath;
           RUST_BACKTRACE = "1";
@@ -40,19 +38,14 @@
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
           '';
 
-          # Add precompiled libraries to rustc search path
           RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-            # Add libraries here (e.g., pkgs.libvmi)
           ]);
 
-          # Add headers to bindgen search path
+		  #You probably Won't need to touch this code for the most part
           BINDGEN_EXTRA_CLANG_ARGS =
-            # Includes normal include path
             (builtins.map (a: ''-I"${a}/include"'') [
               pkgs.glibc.dev
-              # Add dev libraries here (e.g., pkgs.libvmi.dev)
             ])
-            # Includes with special directory paths
             ++ [
               ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
               ''-I"${pkgs.glib.dev}/include/glib-2.0"''
