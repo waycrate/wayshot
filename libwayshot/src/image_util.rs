b1f567e5cf47f82d46a9b925adf0b0f332cf79f0
Andreas Backx <AndreasBackx@users.noreply.github.com>
2025-12-27 14:09:59 +0100

    Do not swap width and height while rotating image buffer as it's already (#256)

--- libwayshot/src/image_util.rs
+++ libwayshot/src/image_util.rs
@@ -7,17 +7,10 @@ use crate::region::Size;
 pub(crate) fn rotate_image_buffer(
     image: DynamicImage,
     transform: Transform,
+    // Includes transform already.
     logical_size: Size,
     max_scale: f64,
 ) -> DynamicImage {
-    // TODO Better document whether width and height are before or after the transform.
-    // Perhaps this should be part of a cleanup of the FrameCopy struct.
-    let (logical_width, _logical_height) = match transform {
-        Transform::_90 | Transform::_270 | Transform::Flipped90 | Transform::Flipped270 => {
-            (logical_size.height, logical_size.width)
-        }
-        _ => (logical_size.width, logical_size.height),
-    };
     let rotated_image = match transform {
         Transform::_90 => image::imageops::rotate90(&image).into(),
         Transform::_180 => image::imageops::rotate180(&image).into(),
@@ -38,15 +31,20 @@ pub(crate) fn rotate_image_buffer(
         _ => image,
     };
 
-    let scale = rotated_image.width() as f64 / logical_width as f64;
+    let rotated_width = rotated_image.width();
+    tracing::trace!(
+        "Rotated width: {rotated_width}, logical width: {}",
+        logical_size.width
+    );
+    let scale = rotated_width as f64 / logical_size.width as f64;
     // The amount of scaling left to perform.
     let scaling_left = max_scale / scale;
+    tracing::debug!("Current scale: {scale}, scaling left (max/current): {scaling_left}");
     if scaling_left <= 1.0 {
         tracing::debug!("No scaling left to do");
         return rotated_image;
     }
 
-    tracing::debug!("Scaling left to do: {scaling_left}");
     let new_width = (rotated_image.width() as f64 * scaling_left).round() as u32;
     let new_height = (rotated_image.height() as f64 * scaling_left).round() as u32;
     tracing::debug!("Resizing image to {new_width}x{new_height}");
