c74b6d11e1ed2a8ba02c00402724637ba41970f4
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-01-17 13:08:11 +0900

    fix: color wrong

--- waymirror-egl/src/shaders/frag.glsl
+++ waymirror-egl/src/shaders/frag.glsl
@@ -7,5 +7,5 @@ in vec2 vTexCoord;
 
 void main() {
    vec4 color = texture(uTexture, vTexCoord);
-   FragColor = vec4 ( 1.0-color.r,1.0-color.g,1.0-color.b, 1.0 );
+   FragColor = vec4 (color.r, color.g, color.b, 1.0 );
 }
