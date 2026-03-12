1018b4915c546cf0039a3812ec5ae2361af28ced
gigas002 <gigas002@pm.me>
2026-03-13 00:20:22 +0900

    feature: add freeze option

--- wayshot/src/config.rs
+++ wayshot/src/config.rs
@@ -43,6 +43,7 @@ impl Config {
 pub struct Base {
     pub output: Option<String>,
     pub cursor: Option<bool>,
+    pub freeze: Option<bool>,
     pub clipboard: Option<bool>,
     pub file: Option<bool>,
     pub stdout: Option<bool>,
@@ -55,6 +56,7 @@ impl Default for Base {
         Base {
             output: None,
             cursor: Some(false),
+            freeze: Some(true),
             clipboard: Some(false),
             file: Some(true),
             stdout: Some(false),
