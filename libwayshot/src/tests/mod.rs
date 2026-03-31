091f44d72e9e276189a54f7495b97597f9b2bc2b
gigas002 <pushkinandrei@team-lab.com>
2026-03-31 11:11:15 +0900

    refactor: rename tests/xxx_tests.rs files to tests/xxx.rs

--- libwayshot/src/tests/mod.rs
+++ libwayshot/src/tests/mod.rs
@@ -1,18 +1,18 @@
 //! Private test module. All unit tests for libwayshot live here to keep main source files focused.
 
-mod convert_tests;
-mod error_tests;
-mod image_util_tests;
-mod screencopy_tests;
+mod convert;
+mod error;
+mod image_util;
+mod screencopy;
 
 #[cfg(all(test, unix))]
-mod output_tests;
+mod output;
 
 #[cfg(all(test, unix))]
-mod region_tests;
+mod region;
 
 #[cfg(all(test, unix))]
-mod dispatch_tests;
+mod dispatch;
 
 #[cfg(all(test, unix))]
-mod lib_tests;
+mod lib;
